7844e0a95f0cb300b138644348b30b8e7ee25c96
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:09:22 +0530

    Add files via upload

---
+++ Method_Parameter/parameter/Sector.java
@@ -0,0 +1,13 @@
+class Sector
+{
+	void area(int r, int angle)
+	{
+		double res= 0.5*r*r*angle;
+		System.out.println(res);
+	}
+	public static void main(String[] args)
+	{
+		Sector S=new Sector();
+		S.area(8,60);
+	}
+}
