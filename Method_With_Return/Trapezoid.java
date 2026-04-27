6ce9bcd3983acf64f10a425999e199211aa7c023
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:28:38 +0530

    Add files via upload

---
+++ Method_With_Return/Trapezoid.java
@@ -0,0 +1,16 @@
+class Trapezoid
+{
+	double area(int a, int b, int h)
+	{
+		double res=0.5*(a+b)*h;
+		//System.out.println(res);
+		return res;
+	}
+	public static void main(String[] args)
+	{
+		Trapezoid T=new Trapezoid();
+		double x=T.area(4,6,8);
+		System.out.println(x);
+	}
+			
+}
