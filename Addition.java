325ddf81537d8a707f6531a5e53ca0cab84d6182
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 22:08:54 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,15 @@
+class Addition
+{
+	static void add()
+	{
+		int a=10;
+		int b=9;
+		int c=5;
+		int d=a+b+c;
+		System.out.println(d);
+	}
+	public static void main(String[] args)
+	{
+		add();
+	}
+}
