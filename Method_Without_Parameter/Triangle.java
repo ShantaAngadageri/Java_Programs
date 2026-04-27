761221807c5c3f170f6bc0c4b5c4285eebdabd45
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:21:52 +0530

    Add files via upload

---
+++ Method_Without_Parameter/Triangle.java
@@ -0,0 +1,15 @@
+class Triangle{
+	void area(){
+		int a=15;
+		int b=60;
+		double res= 0.5*a*b;
+		System.out.println(res);
+	}
+	public static void main(String[] args)
+	{
+		Triangle T=new Triangle();
+		T.area();
+	}
+}
+
+	
