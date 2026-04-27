89b4e324d225a49c194b752bdd68753004d759ae
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 19:59:53 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,10 @@
+class Square {
+    static void area(int s) {
+        int area = s * s;
+        System.out.println(area);
+    }
+
+    public static void main(String[] args) {
+        area(6);
+    }
+}
