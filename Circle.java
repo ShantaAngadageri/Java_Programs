eed5a776087f1afba937593699d32ec59bf146fc
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 19:55:28 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,10 @@
+class Circle {
+    static void area(int r) {
+        double area = 3.14 * r * r;
+        System.out.println(area);
+    }
+
+    public static void main(String[] args) {
+        area(7);
+    }
+}
