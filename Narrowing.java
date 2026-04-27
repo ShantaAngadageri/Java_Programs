a9cc70ddd748f8575dcb938f34d2bbf6d157b6f0
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 22:00:20 +0530

    Add files via upload

---
+++ Narrowing.java
@@ -0,0 +1,18 @@
+public class Narrowing
+{
+
+	public static void main(String[] args) {
+		double a=256.98;
+		int b=(int)a;
+		System.out.println(b);
+		float c=567.8f;
+		short d=(short)c;
+		System.out.println(d);
+		long z=100000;
+		byte g=(byte)z;
+		char ch='A';
+		System.out.println(ch);
+
+	}
+
+}
