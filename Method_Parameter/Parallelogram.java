e2bdeb22e8c04a80094bb43626d2ad4c37648530
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 20:08:42 +0530

    Add files via upload

---
+++ Method_Parameter/Parallelogram.java
@@ -0,0 +1,13 @@
+class Parallelogram
+{
+	void area(int b, int h)
+	{
+
+		int res=b*h;
+		System.out.println(res);
+	}
+	public static void main(String[] args)
+	{
+		new Parallelogram().area(6,8);
+	}
+}
