2e2d96a8b221464117a3c049355478c4b34f071c
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:18:46 +0530

    Add files via upload

---
+++ Method_Without_Parameter/Sample3.java
@@ -0,0 +1,17 @@
+class Parallelogram{
+	void area()
+	{
+		int b=25;
+		int h=20;
+		int res=b*h;
+		System.out.println(res);
+	}
+}
+class Sample3
+{
+	public static void main(String[] args)
+	{
+		Parallelogram P=new Parallelogram();
+		P.area();
+	}
+}
