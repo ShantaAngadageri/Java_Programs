9c6273ceb548205ce400b9c2d82311aa88594dc1
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 20:11:31 +0530

    Add files via upload

---
+++ Method_Parameter/Triangle.java
@@ -0,0 +1,13 @@
+class Triangle{
+	void area(int a, int b){
+
+		int res=o.5*a*b;
+		System.out.println(res);
+	}
+	public static void main(String[] args)
+	{
+		new Triangle().area(4,6);
+	}
+}
+
+	
