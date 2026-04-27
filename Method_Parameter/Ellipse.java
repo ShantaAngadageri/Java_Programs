b8d6589a76b63d319087a8dd84186cb7c4ff8fbd
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 20:59:34 +0530

    Add files via upload

--- Method_Parameter/Ellipse.java
+++ Method_Parameter/Ellipse.java
@@ -2,12 +2,12 @@ class Ellipse
 {
 	void area(int a, int b)
 	{
-
 		double pi=3.142;
 		double res=pi*a*b;
 		System.out.println(res);
 	}
 	public static void main(String[] main){
-		new Ellipse().area(12,14);
+		Ellipse E=new Ellipse();
+		E.area(12,18);
 	}
 }
