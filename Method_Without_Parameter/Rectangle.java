4b89ce95aae075fbff8a10da22f68f846f5cecff
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:17:26 +0530

    Add files via upload

---
+++ Method_Without_Parameter/Rectangle.java
@@ -0,0 +1,14 @@
+class Rectangle
+{
+	void area(){
+		int w=8;
+		int h=9;
+		int res=w*h;
+		System.out.println(res);
+	}
+	public static void main(String[]  args)
+	{
+		Rectangle R=new Rectangle();
+		R.area();
+	}
+}
