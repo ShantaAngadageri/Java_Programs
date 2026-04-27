f400d46b5198d04df01bfb5858190ff6fce3696c
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 19:58:52 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+class Rectangle {
+    static void area(int l, int b) {
+        int area = l * b;
+        System.out.println(area);
+    }
+
+    public static void main(String[] args) {
+        area(7, 8);
+    }
+}
