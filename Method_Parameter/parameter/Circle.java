47a5dcf8648524052b813da613ca1ebab672f2b8
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:02:21 +0530

    Add files via upload

---
+++ Method_Parameter/parameter/Circle.java
@@ -0,0 +1,16 @@
+class Circle
+{
+	void area(int r)
+	{
+		
+		double pi=3.142;
+		double res=pi*r*r;
+		System.out.println(res);
+	}
+	public static void main(String[] args)
+	{
+		Circle C=new Circle();
+		C.area(16);
+	}
+	
+}
