fb493dff4a05fcd51803c97a547b157f6e229506
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 22:08:04 +0530

    Add files via upload

---
+++ Widening.java
@@ -0,0 +1,22 @@
+public class Widening {
+
+	public static void main(String[] args) {
+		byte a= 54;
+		long b=a;
+		System.out.println(b);
+		short c=100;
+		float d= c;
+		System.out.println(d);
+		int x= 256;
+		double y=x;
+		System.out.println(y);
+		char ch='A';
+		System.out.println(ch);
+		if(5>=4){
+			System.out.print(true);
+		}else {
+		System.out.print(false);
+		}
+	}
+
+}
