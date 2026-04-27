1a0c95fc75436cb8e5a80c9a9d90242644794955
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 19:58:09 +0530

    Add files via upload

---
+++ Parallelogram.java
@@ -0,0 +1,10 @@
+class Parallelogram {
+    static void area(int b, int h) {
+        int area = b * h;
+        System.out.println(area);
+    }
+
+    public static void main(String[] args) {
+        area(10, 5);
+    }
+}
