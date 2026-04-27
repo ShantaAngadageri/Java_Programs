cf0f704be778c144cbe5d7c511ad7a5caf9e3668
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 19:56:45 +0530

    Add files via upload

---
+++ Ellipse.java
@@ -0,0 +1,10 @@
+class Ellipse {
+    static void area(int a, int b) {
+        double area = 3.14 * a * b;
+        System.out.println(area);
+    }
+
+    public static void main(String[] args) {
+        area(5, 3);
+    }
+}
