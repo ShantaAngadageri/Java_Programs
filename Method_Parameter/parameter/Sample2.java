104230d725ffd1e13184aa457354377b44ec88db
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:05:39 +0530

    Add files via upload

---
+++ Method_Parameter/parameter/Sample2.java
@@ -0,0 +1,15 @@
+class Square{
+	void area(int a){
+		//int a=10;
+		int res=a*a;
+		System.out.println(res);
+	}
+}
+class Sample2
+{
+	public static void main(String[] args)
+	{
+	   Square S=new Square();
+	   S.area(8);
+	}
+}
