c66f28796023f5a36ed1a295446047d67164dfbf
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:17:44 +0530

    Add files via upload

---
+++ Method_Without_Parameter/Sample.java
@@ -0,0 +1,18 @@
+class Triangle{
+	void area(){
+		int a=15;
+		int b=60;
+		double res= 0.5*a*b;
+		System.out.println(res);
+	}
+}
+class Sample{
+
+	public static void main(String[] args)
+	{
+		Triangle T=new Triangle();
+		T.area();
+	}
+}
+
+	
