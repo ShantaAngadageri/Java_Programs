6b61d2592362ac6a39145064407fcbc113929225
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:04:30 +0530

    Add files via upload

---
+++ Method_Parameter/parameter/Sample.java
@@ -0,0 +1,16 @@
+class Rectangle
+{	
+	void area(int w, int h){
+		int res=w*h;
+		System.out.println(res);
+	}
+}
+class Sample
+{
+
+	public static void main(String[]  args)
+	{
+		Rectangle R=new Rectangle();
+		R.area(8,9);
+	}
+}
