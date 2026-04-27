117d60279518333edf363b5f6475f26e19fd70ad
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 20:00:36 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,10 @@
+class Triangle {
+    static void area(int b, int h) {
+        double area = 0.5 * b * h;
+        System.out.println(area);
+    }
+
+    public static void main(String[] args) {
+        area(7, 8);
+    }
+}
