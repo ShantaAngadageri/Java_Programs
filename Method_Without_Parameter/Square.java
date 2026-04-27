6858ddfaae5e20a89930bcbb47949c5863e42e0e
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:20:51 +0530

    Add files via upload

---
+++ Method_Without_Parameter/Square.java
@@ -0,0 +1,13 @@
+class Square
+{
+	void area(){
+		int a=10;
+		int res=a*a;
+		System.out.println(res);
+	}
+	public static void main(String[] args)
+	{
+	   Square S=new Square();
+	   S.area();
+	}
+}
