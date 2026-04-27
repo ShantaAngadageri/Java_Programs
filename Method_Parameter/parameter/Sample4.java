7893d0509fe7f685e4d4c8f30c777991ca726a05
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:07:00 +0530

    Add files via upload

---
+++ Method_Parameter/parameter/Sample4.java
@@ -0,0 +1,17 @@
+class Trapezoid{
+	
+	void area(int a, int b,int h)
+	{
+		double res=0.5*(a+b)*h;
+		System.out.println(res);
+	}
+}
+class Sample4
+{
+	public static void main(String[] args)
+	{
+		Trapezoid T=new Trapezoid();
+		T.area(4,6,8);
+	}
+			
+}
