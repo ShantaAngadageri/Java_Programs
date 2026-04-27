1c90ed1403fe8a75c140f6955db982e3c94a3f42
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 21:44:29 +0530

    Add files via upload

--- OOPS_Related/Heirarchical.java
+++ OOPS_Related/Heirarchical.java
@@ -1,31 +1,32 @@
-class Animal {
-    void eat() {
-        System.out.println("Eating...");
-    }
+class Sample
+{
+	void disp(){
+		System.out.println("Hii");
 }
-
-class Dog extends Animal {
-    void bark() {
-        System.out.println("Barking...");
-    }
 }
-
-class Cat extends Animal {
-    void meow() {
-        System.out.println("Meowing...");
-    }
+class Demo extends Sample
+{
+	void cool(){
+		System.out.println("Hello");
+	}
 }
-
-public class Heirarchical {
-    public static void main(String[] args) {
-
-        Dog d = new Dog();
-        Cat c = new Cat();
-
-        d.eat();   // inherited from Animal
-        d.bark();  // Dog method
-
-        c.eat();   // inherited from Animal
-        c.meow();  // Cat method
-    }
+class Tester extends Sample
+{
+	void test(){
+		System.out.println("Java");
+	}
+}
+class Heirarchical
+{
+	public static void main(String[] args)
+	{
+		Tester d1=new Tester();
+		d1.disp();
+		d1.test();
+		Demo d2=new Demo();
+		d2.cool();
+		d2.disp();
+		
+		
+	}
 }
