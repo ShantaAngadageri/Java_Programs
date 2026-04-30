0b5aef32db565af97dfeb8a5036e80d43334f753
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-30 23:10:25 +0530

    Implement arrow function to calculate area

---
+++ Arrow Function/Sample2.java
@@ -0,0 +1,21 @@
+package hellojava;
+
+interface Demo2
+{
+	double disp();
+}
+public class Sample2 {
+
+	public class Sample2 {
+		public static void main(String[] args) {
+	 	   Demo2 d2=() -> { 
+	 		  int b=10;
+   		   int h=15;
+   		    double res=0.5*b*h;
+	 		   return res;
+	 		};
+	 		d2.disp(); 
+	    }
+	}
+
+}
